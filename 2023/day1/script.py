01435c3464068587095cc1bc251225cc83ee2d77
baalooos <charles-edouard.gagnaire@alterway.fr>
2024-12-06 12:13:14 +0100

    push old code

--- 2023/day1/script.py
+++ 2023/day1/script.py
@@ -1,7 +1,8 @@
 f = open("input.txt", "r")
 
-digit = [{O, zero}, {1, }]
+digit = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
+digit_translate = [{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}]
 tab = []
 sum = 0
 
